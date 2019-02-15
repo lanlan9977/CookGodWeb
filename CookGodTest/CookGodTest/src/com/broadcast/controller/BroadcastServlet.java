12d0ad8cefe75b8e0fb1cce9fd6715960ac89232
lanlan9977 <lanlan9977@gmail.com>
2019-02-15 22:31:16 +0800

    CookGodDB

---
+++ CookGodTest/CookGodTest/src/com/broadcast/controller/BroadcastServlet.java
@@ -0,0 +1,21 @@
+package com.broadcast.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BroadcastServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		doPost(req, res);
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		res.setCharacterEncoding("utf-8");
+
+		
+	}
+
+}
